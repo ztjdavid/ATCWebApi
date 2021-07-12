2875d61509598ecc47dcac5b70cdb84ce6b4e612
Gare_TH <329288574@qq.com>
2021-07-12 18:27:39 +0800

    remove unnecessary @NotNull annotation

--- src/main/java/garethcxy/tk/ATC/Entity/RegularFeeObject.java
+++ src/main/java/garethcxy/tk/ATC/Entity/RegularFeeObject.java
@@ -4,21 +4,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 public class RegularFeeObject {
-    @NotNull
     @JsonProperty(value = "electricityFee")
     private double electricityFee;
-    @NotNull
     @JsonProperty(value = "amazonFee")
     private double amazonFee;
-    @NotNull
     @JsonProperty(value = "internetFee")
     private double internetFee;
-    @NotNull
     @JsonProperty(value = "otherFee")
     private double otherFee;
 
