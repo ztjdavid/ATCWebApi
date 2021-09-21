16a06af46e1a0dfdb58b23a94ab9bfb7e565094e
GareArc <cxy329288574@gmail.com>
2021-09-20 21:24:48 -0400

    fix the bug that mail fails to load config file.

--- src/main/java/garethcxy/tk/ATC/Resource/OrderResource.java
+++ src/main/java/garethcxy/tk/ATC/Resource/OrderResource.java
@@ -40,5 +40,5 @@ public class OrderResource {
     }
 
     @PostMapping("/order/clear")
-    public boolean clear(){ return orderService.clear(); }iugvbiygyiuguigbiu
+    public boolean clear(){ return orderService.clear(); }iugvbiygyiuguigbiugiuhiuhouiiuhuoi
 }
